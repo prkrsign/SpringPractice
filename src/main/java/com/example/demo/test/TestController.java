3aa89957e065e2371fe336f8bf3bead8d1222914
prkrsign <prkrsign@yahoo.co.jp>
2019-12-31 04:19:50 +0900

    connect mysql

--- src/main/java/com/example/demo/test/TestController.java
+++ src/main/java/com/example/demo/test/TestController.java
@@ -1,19 +1,27 @@
 package com.example.demo.test;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+
 @Controller
 @RequestMapping("/")
 public class TestController {
 	
-	@GetMapping
-	public String test(Model model) {
-		model.addAttribute("title", "SpringBlog");
-		return "test";
-	}
-	
-
+    @Autowired
+    EmployeeRepository empRepository;
+    
+    @GetMapping
+    public String index(Model model) {
+        List<Employee> emplist=empRepository.findAll();
+        model.addAttribute("emplist", emplist);
+        return "test";
+    }
 }
