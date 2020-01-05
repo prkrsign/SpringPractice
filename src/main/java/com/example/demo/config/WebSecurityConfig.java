9b746fdbaf39c2cee9f1cc452a67fb80070a655b
prkrsign <prkrsign@yahoo.co.jp>
2020-01-05 21:00:47 +0900

    view index-page and link login-page

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,23 +1,60 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        //ログインページを指定。
-        //ログインページへのアクセスは全員許可する。
-        http.formLogin()
-            .loginPage("/login")
-            .permitAll();
+	@Override
+	  protected void configure(HttpSecurity http) throws Exception {
+	    http
+	      .authorizeRequests()
+	        // アクセス制限のないURLを指定
+	        .antMatchers("/").permitAll()
+	        // そのほかは認証済みでしかアクセスできない
+	        .anyRequest().authenticated()
+	        .and()
+	       // ログイン画面の設定 アクセス制限なし
+	      .formLogin()
+	        .loginPage("/login")
+	        .permitAll()
+	        .and()
+	      .logout()
+	        .permitAll();
+	  }
 
-        http.authorizeRequests()
-            .anyRequest().authenticated();
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // パスワード設定。公式チュートリアルの記述だとエラーが発生
+        String password = passwordEncoder().encode("password");
+
+        // インメモリの認証を行うための設定
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("user").password(password).roles("USER");
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                            "/images/**",
+                            "/css/**",
+                            "/javascript/**"
+                            );
     }
 }
